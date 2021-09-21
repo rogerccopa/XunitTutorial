933ae0b26b5ba430124912821db3757e57fe9b14
rogerccopa <rogerccopa@gmail.com>
2021-09-21 17:35:13 -0500

    Catch ArgumentException

---
+++ DemoLibrary.Tests/ExamplesTests.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Xunit;
+
+namespace DemoLibrary.Tests
+{
+    public class ExamplesTests
+    {
+        [Fact]
+        public void ExampleLoadTextFile_ValidNameShouldWork()
+        {
+            string actual = Examples.ExampleLoadTextFile("This is a valid file name.");
+
+            Assert.True(actual.Length > 0);
+        }
+
+        [Fact]
+        public void ExampleLoadTextFile_InvalidNameShouldFail()
+        {
+            // test that ArgumentException is thrown on parameter 'file'
+            Assert.Throws<ArgumentException>("file", () => Examples.ExampleLoadTextFile(""));
+        }
+    }
+}
