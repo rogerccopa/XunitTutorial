8de0f0d76562ce62579d1f17eaebd3e3783b3f0e
rogerccopa <rogerccopa@gmail.com>
2021-09-23 15:30:06 -0500

    Add person to people list

---
+++ DemoLibrary.Tests/DataAccessTests.cs
@@ -0,0 +1,25 @@
+﻿using DemoLibrary.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace DemoLibrary.Tests
+{
+    public class DataAccessTests
+    {
+        [Fact]
+        public void AddPersonToPeopleList_ShouldWork()
+        {
+            PersonModel newPerson = new PersonModel { FirstName = "Roger", LastName = "Ccopa" };
+            List<PersonModel> people = new List<PersonModel>();
+
+            DataAccess.AddPersonToPeopleList(people, newPerson);
+
+            Assert.True(people.Count == 1);
+            Assert.Contains<PersonModel>(newPerson, people);
+        }
+    }
+}
